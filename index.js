9961b989621155cf025838f6e6f79cba8c85b9a6
Sergio Medina <hola@sergiomedina.io>
2022-10-14 22:31:22 -0300

    feat: select pokemon abilities

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ const consultarPokemon = async (nombrePokemon) => {
     let response = await fetch(`${url}/pokemon/${nombrePokemon}`);
 
     if (response.ok) { 
-    let json = await response.json();
-    console.log(json)
+    let pokemon = await response.json();
+    const abilitiesList = pokemon.abilities;
+
+    const modifAbilitiesList = abilitiesList
+        .map((ability) => {
+            return ability;
+        })
+
+    console.log(modifAbilitiesList)
+    
     } else {
     alert("HTTP-Error: " + response.status);
     }
