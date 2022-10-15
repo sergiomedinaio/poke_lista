30cf2259b9b3ed373ff681f227119d48aa6b48d2
Sergio Medina <hola@sergiomedina.io>
2022-10-14 22:24:02 -0300

    refactor: update pokemon name

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const url = "https://pokeapi.co/api/v2";
 
 //async es capaz de trabajar con promises
-const consultarPokemon = async (pokemon) => {
-    let response = await fetch(`${url}/pokemon/${pokemon}`);
+const consultarPokemon = async (nombrePokemon) => {
+    let response = await fetch(`${url}/pokemon/${nombrePokemon}`);
 
     if (response.ok) { 
     let json = await response.json();
